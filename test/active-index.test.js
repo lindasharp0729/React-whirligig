670939fe012c7dc18a3e861d3973a419ec46a139
LindaSharp <lindasharp0729@gmail.com>
2026-02-06 12:20:00 +0300

    code

--- test/active-index.test.js
+++ test/active-index.test.js
@@ -19,7 +19,7 @@ test('activeIndex state', () => {
           {(_next, _prev) => {
             goNext = _next
             goPrev = _prev
-            return [0, 1, 2, 3, 4, 5, 6, 7]
+            return [0,  1,  2,  3,  4, 5,  6,  7]
           }}
         </Track>
       ),
@@ -30,16 +30,12 @@ test('activeIndex state', () => {
   expect(ai({ startAt: 3 }).component.state('activeIndex')).toBe(3)
   const nexted = ai({ visibleSlides: 3 })
   nexted.next()
-
   expect(nexted.component.state('activeIndex')).toBe(3)
   nexted.next()
   expect(nexted.component.state('activeIndex')).toBe(5)
-
   const preved = ai({ visibleSlides: 3, startAt: 5 })
   preved.prev()
-
   expect(preved.component.state('activeIndex')).toBe(2)
   preved.prev()
-  
   expect(preved.component.state('activeIndex')).toBe(0)
 })
--- test/utils.test.js
+++ test/utils.test.js
@@ -5,17 +5,14 @@ test('utils.includes', () => {
   expect(u.includes(1, [0, 2, 3])).toBe(false)
 })
 
-
 test('utils.values', () => {
   const o = { a: 1, b: 2, c: 3, d: 4 }
   const e = [1, 2, 3, 4]
   expect(u.values(o)).toEqual(e)
 })
-
 test('utils.minMap', () => {
   expect(u.minMap(1, 2, 3, 4)(5)).toBe(1)
 })
-
 
 test('utils.maxMap', () => {
   expect(u.maxMap(1, 2, 3, 4)(5)).toBe(5)
